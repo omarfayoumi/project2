2dc0893ff2e6f6f2f89e923b7699bc0fbd92d7b8
omarfayoumi <12133641@students.liu.edu.lb>
2025-01-05 00:45:08 +0200

    Add files via upload

---
+++ lib/ViewFootball.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+final List<String> _customers = [];
+// domain of your server
+const String _baseURL = 'fdb1029.awardspace.net';
+
+class ShowCustomersF extends StatefulWidget {
+  const ShowCustomersF({super.key});
+
+  @override
+  State<ShowCustomersF> createState() => _ShowCustomersState();
+}
+
+class _ShowCustomersState extends State<ShowCustomersF> {
+  bool _load = false;
+
+  void update(bool success) {
+    setState(() {
+      _load = true; // show product list
+      if (!success) { // API request failed
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('failed to load data')));
+      }
+    });
+  }
+
+
+  @override
+  void initState() {
+    // update data when the widget is added to the tree the first tome.
+    updateCustomers(update);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Available Customers'),
+          centerTitle: true,
+        ),
+        body: _load ? const ListCustomers() : const Center(
+            child: SizedBox(width: 100, height: 100, child: CircularProgressIndicator())
+        )
+    );
+  }
+}
+
+class ListCustomers extends StatelessWidget {
+  const ListCustomers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: _customers.length,
+        itemBuilder: (context, index) => Column(children: [
+          const SizedBox(height: 10),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Flexible(child: Text(_customers[index], style: const TextStyle(fontSize: 18)))
+          ])
+        ])
+    );
+  }
+}
+
+void updateCustomers(Function(bool success) update) async {
+  try {
+    final url = Uri.https(_baseURL, 'getfootball.php');
+    final response = await http.get(url)
+        .timeout(const Duration(seconds: 5)); // max timeout 5 seconds
+    _customers.clear(); // clear old products
+    if (response.statusCode == 200) { // if successful call
+      final jsonResponse = convert.jsonDecode(response.body); // create dart json object from json array
+      for (var row in jsonResponse) { // iterate over all rows in the json array
+        _customers.add('pid: ${row['pid']} name: ${row['name']} age: ${row['age']} cid: ${row['cid']}');
+      }
+      update(true); // callback update method to inform that we completed retrieving data
+    }
+  }
+  catch(e) {
+    update(false); // inform through callback that we failed to get data
+  }
+}
+
